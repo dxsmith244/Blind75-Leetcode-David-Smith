11d4c936d74b0a02bea465dec093c77ea985e1f0
dxsmith244 <dxsmith244@gmail.com>
2024-10-29 11:40:57 -0400

    feat:updates file structure and adds requested changes

---
+++ Blind75/src/main/java/com/dxsmith244/blind75/twosum/Twosum.java
@@ -0,0 +1,33 @@
+package com.dxsmith244.blind75;
+import java.util.HashMap;
+
+public class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+class TimeOpitmalTwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
